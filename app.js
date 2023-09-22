5be4aa238412c17101cb13fb708cada21584e7e2
ahmedraza1234567890 <razamemon9900@gmail.com>
2023-09-22 20:16:50 +0500

    create file

---
+++ app.js
@@ -0,0 +1,6 @@
+var con = "Hello... I can run JS throngh my web browser's console"
+alert("Error! Please enter a valid password");
+alert("Wellcome to JS Land...\nHappy Coding!");
+alert ("Welcome to JS Land...");
+alert (con)
+console.log(con)
