7adf487d6f33c2ad2a67f63550c2faddf9b4f8d3
ahmedraza1234567890 <razamemon9900@gmail.com>
2023-09-22 20:19:31 +0500

    worked

--- app.js
+++
@@ -1,6 +0,0 @@
-var con = "Hello... I can run JS throngh my web browser's console"
-alert("Error! Please enter a valid password");
-alert("Wellcome to JS Land...\nHappy Coding!");
-alert ("Welcome to JS Land...");
-alert (con)
-console.log(con)
